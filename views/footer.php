8d56cc55c1dfd8c9bad49e726e54953c32ce29b0
ALVARO CEDAZO GONZALEZ <6002510@viewnext.com>
2023-11-17 13:33:46 +0100

    Creacion de proyecto y primeros cambios.

---
+++ views/footer.php
@@ -0,0 +1,27 @@
+    <footer>
+        <div class="columnas">
+            <div>
+                <a href="#">Sobre Nosotros</a>
+                <ul>
+                    <li><a href="#">Información de la empresa</a></li>
+                    <li><a href="#">Equipo</a></li>
+                    <li><a href="#">Logros y Reconocimientos</a></li>
+                    <li><a href="#">Filosofía de negocio</a></li>
+                    <li><a href="#">Ubicación</a></li>
+                </ul>
+            </div>
+            <div>
+                <a href="#">Contacto</a>
+                <ul>
+                    <li><a href="#">Formulario de Contacto</a></li>
+                    <li><a href="#">Información de contacto</a></li>
+                    <li><a href="#">Horario de atención</a></li>
+                    <li><a href="#"></a>Ubicación</li>
+                </ul>
+            </div>
+        </div>
+        <div>Creado por Álvaro Cedazo González &copy;</div>
+    </footer>
+    <script src="static/js/app.js"></script>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,2 @@
+<?php require("views/parts/head.php"); ?>
+
